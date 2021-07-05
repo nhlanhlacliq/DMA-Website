ed6ae59cc1967e823856b5788d969592ef2aedb3
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-07-05 15:15:56 +0200

    add section link fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 // JavaScript Document
- $(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $("#autoWidth").removeClass("cS-hidden");
+    },
   });
+});
+
+// var shiftWindow = function () {
+//   scrollBy(0, -150);
+// };
+// if (location.hash) shiftWindow();
+// window.addEventListener("hashchange", shiftWindow);
