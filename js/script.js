ed712c521482589883c23182e6f2292f449b30e8
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-07-05 19:02:19 +0200

    remove commented code

--- js/script.js
+++ js/script.js
@@ -8,9 +8,3 @@ $(document).ready(function () {
     },
   });
 });
-
-// var shiftWindow = function () {
-//   scrollBy(0, -150);
-// };
-// if (location.hash) shiftWindow();
-// window.addEventListener("hashchange", shiftWindow);
