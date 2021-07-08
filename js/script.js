9547bb54785278d16d649bbeccfd8d246d044409
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-07-08 15:11:49 +0200

    add active link functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+//LightSlider
 $(document).ready(function () {
   $("#autoWidth").lightSlider({
     autoWidth: true,
@@ -8,3 +9,18 @@ $(document).ready(function () {
     },
   });
 });
+
+// Get the container element
+var linkContainer = document.getElementById("menu");
+
+// Get all buttons with class="nav-link" inside the container
+var links = linkContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
